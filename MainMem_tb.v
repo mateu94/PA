b13b53cbfd255f7e9ce16f4855c35be54eecb7d2
DiyaJ <32401605+DiyaJ@users.noreply.github.com>
2017-12-06 21:23:52 +0100

    25 bit address

--- MainMem_tb.v
+++ MainMem_tb.v
@@ -1,19 +1,25 @@
 `timescale 1ns / 1ps
 module MainMem_testbench();
+//Parameters
+parameter Data_Width = 128;
+parameter Addr_Width = 25;
+parameter RamDepth =  1<<Addr_Width;
+
 reg clk,CS,OE,WE;
-reg[31:0] Addr;
-wire [7:0] Data;
-wire [7:0] Data_out;
+wire Ready_Mem;
+reg[Addr_Width-1:0] Addr;
+wire [Data_Width-1:0] Data;
+wire [Data_Width-1:0] Data_out;
 wire oe_r;
 
 //To make the inout port (Data) testable
-wire [7:0] Data_input;
-reg [7:0] Data_output;
+wire [Data_Width-1:0] Data_input;
+reg [Data_Width-1:0] Data_output;
 reg Data_output_valid;
 assign Data_input = Data;
-assign Data = (Data_output_valid==1)?Data_output: 8'hZZ;
+assign Data = (Data_output_valid==1)?Data_output: 128'bZ;
 
-MainMem test(clk, CS, OE, WE, Addr, Data);
+MainMem test(clk, CS, OE, WE, Addr, Data,Ready_Mem);
 
 //creating clock 
 always #10 clk= ~clk; 
@@ -26,44 +32,54 @@ initial
     WE= 1'b0; 
     Addr= 32'h0;
     #20
-// write 'h5' into address 1
+// write 'hFFFFFFFFFFFFF' into address 1
     Data_output_valid=1'b1;
     CS= 1'b1;
     WE= 1'b1;
     Addr= 32'h1;
-    Data_output = 8'h5;
-    #20
-// write 'h6' into address 2
+    Data_output = 128'hFFFFFFFFFFFFF;
+    #100
+// write 'h76' into address 2
     Data_output_valid=1'b1;
     CS= 1'b1;
     WE= 1'b1;
     Addr= 32'h2;
-    Data_output = 8'h76;
-    #20
-// write 'h76' into address FFFFFFFF
+    Data_output = 128'h76;
+    #100
+// write 'h555' into address 1FFFFFF
     Data_output_valid=1'b1;
     CS= 1'b1;
     WE= 1'b1;
-    Addr= 32'hFFFFFFFF;
-    Data_output = 8'h76;
-    #20
+    Addr= 32'h1FFFFFF;
+    Data_output = 128'h555;
+    #100
+//put WE=0
+    WE= 1'b0;
+    #50
 // read from address 1
     Data_output_valid=1'b0;
     WE= 1'b0;
     OE= 1'b1;
     Addr= 32'h1;
-    #20
+    #110
 // read from address 2     
     Data_output_valid=1'b0;
     WE= 1'b0;
     OE= 1'b1;
     Addr= 32'h2;
-#20
+    #110
+// read from address 1FFFFFF    
+    Data_output_valid=1'b0;
+    WE= 1'b0;
+    OE= 1'b1;
+    Addr= 32'h1FFFFFF;
+
+    #110
 // read from address FFFFFFFF    
     Data_output_valid=1'b0;
     WE= 1'b0;
     OE= 1'b1;
-    Addr= 32'hFFFFFFFF;
+    Addr= 32'hFFFFFFF;   
     
   end
 endmodule
